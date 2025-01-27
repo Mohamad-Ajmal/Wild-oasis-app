3110bdef831783b615bf5bee39e0095ae6db135a
Mohamad-Ajmal <Ajmalibrahimkhail121@gmail.com>
2025-01-27 09:37:33 +0430

    Filtering cabins functionality is added to cabin page

--- src/ui/Filter.jsx
+++ src/ui/Filter.jsx
@@ -1,3 +1,6 @@
+/* eslint-disable react/prop-types */
+import React from "react";
+import { useSearchParams } from "react-router-dom";
 import styled, { css } from "styled-components";
 
 const StyledFilter = styled.div`
@@ -33,3 +36,28 @@ const FilterButton = styled.button`
     color: var(--color-brand-50);
   }
 `;
+
+
+export default function Filter({filterFiled, options}) {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const currentFilter = searchParams.get(filterFiled) || options.at(0).value
+
+  function handleClick(value){
+    searchParams.set(filterFiled, value);
+    setSearchParams(searchParams);
+  }
+  return (
+    <StyledFilter>
+      {options.map((option) => (
+          <FilterButton 
+            key={option.value} 
+            onClick={() => handleClick(option.value)}
+            active={option.value === currentFilter}
+            >{option.label}
+            
+          </FilterButton>
+        ))}
+    </StyledFilter>
+  )
+}
+
