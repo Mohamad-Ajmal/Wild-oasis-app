ab7a8f2c43012dc0afea28b846fce0bf3f8a290c
Mohamad-Ajmal <Ajmalibrahimkhail121@gmail.com>
2025-01-28 11:04:09 +0430

    Building the single booking page

--- src/features/bookings/BookingDataBox.jsx
+++ src/features/bookings/BookingDataBox.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import styled from "styled-components";
 import { format, isToday } from "date-fns";
 import {
--- src/features/bookings/BookingRow.jsx
+++ src/features/bookings/BookingRow.jsx
@@ -6,6 +6,9 @@ import Table from "../../ui/Table";
 
 import { formatCurrency } from "../../utils/helpers";
 import { formatDistanceFromNow } from "../../utils/helpers";
+import Menus from "../../ui/Menus";
+import { HiEye } from "react-icons/hi2";
+import { useNavigate } from "react-router-dom";
 
 const Cabin = styled.div`
   font-size: 1.6rem;
@@ -13,7 +16,7 @@ const Cabin = styled.div`
   color: var(--color-grey-600);
   font-family: "Sono";
 `;
-
+ 
 const Stacked = styled.div`
   display: flex;
   flex-direction: column;
@@ -48,6 +51,7 @@ function BookingRow({
     cabins: { name: cabinName },
   },
 }) {
+  const navigate = useNavigate();
 
   const statusToTagName = {
     unconfirmed: "blue",
@@ -80,6 +84,13 @@ function BookingRow({
       <Tag type={statusToTagName[status]}>{status.replace("-", " ")}</Tag>
 
       <Amount>{formatCurrency(totalPrice)}</Amount>
+
+      <Menus.Menu>
+        <Menus.Toggle id={bookingId} />
+        <Menus.List id={bookingId}>
+          <Menus.Button icon={<HiEye />} onClick={() => navigate(`/bookings/${bookingId}`)}>See details</Menus.Button>
+        </Menus.List>
+      </Menus.Menu>
     </Table.Row>
   );
 }
