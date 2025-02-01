03c377ba774c24100d92c987c40a568a01ac189b
Mohamad-Ajmal <Ajmalibrahimkhail121@gmail.com>
2025-02-01 11:40:25 +0430

    Authentication user Login with Supabase

--- src/ui/Heading.jsx
+++ src/ui/Heading.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import styled, { css } from "styled-components";
 
 
@@ -17,7 +18,11 @@ ${(props) => props.as === "h3" && css `
     font-size: 2rem;
     font-weight: 600;
 `}
-
+${(props) => props.as === "h4" && css `
+    font-size: 3rem;
+    font-weight: 600;
+    text-align: center;
+`}
 line-height: 1.4;
 `;
 
