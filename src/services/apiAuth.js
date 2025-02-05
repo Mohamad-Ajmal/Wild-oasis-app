4d4cf8e75478ad0cd80d8de28dceb3285d366cc9
Mohamad-Ajmal <Ajmalibrahimkhail121@gmail.com>
2025-02-05 10:48:45 +0430

    Authorization protecting route

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -13,3 +13,17 @@ const { data, error } = await supabase.auth.signInWithPassword({
 
   
 }
+
+export async function getCurrentUser() {
+    const { data: session } = await supabase.auth.getSession();
+
+    if(!session.session) return null;
+
+
+    const {data, error} = await supabase.auth.getUser();
+
+    if(error) throw new Error(error.message);
+
+    return data?.user;
+    
+}
---
+++ src/ui/ProtectedRoute.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect } from 'react'
+import { useUser } from '../features/authentication/useUser';
+import Spinner from './Spinner';
+import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+
+const fullPage = styled.div `
+    height: 100vh;
+    background-color: var(--color-grey-50);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+export default function ProtectedRoute({children}) {
+    const navigate = useNavigate();
+    // 1. Load the authenticated user
+
+    const {user, isLoading,isAuthenticated} = useUser();
+
+    // 2. if there is No authenticated user, redirect to the login
+    useEffect(function(){
+        if(!isAuthenticated && !isLoading) navigate("/login");
+    },[isAuthenticated, isLoading, navigate])
+
+
+     // 3. while loading, show a Spinner
+     if(isLoading) 
+        return (
+            <fullPage>
+                <Spinner />
+            </fullPage>
+        );
+        
+    // 4. if there IS a user, render the app
+    if(isAuthenticated) return children;
+}
